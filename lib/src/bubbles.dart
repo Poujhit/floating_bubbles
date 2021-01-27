b845113066ad6d0942a60784c092f06270843290
Poujhit <poujhit.dev@gmail.com>
2021-01-27 18:00:06 +0530

    feat:added duration field for the animation

--- lib/src/bubbles.dart
+++ lib/src/bubbles.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -22,6 +23,12 @@ class FloatingBubbles extends StatefulWidget {
   /// Typically it should be > 0 and < 0.5. Otherwise the bubble size will be too large.
   final double sizeFactor;
 
+  /// Number of [Seconds] the animation needs to draw on the screen.
+  /// [duration] is has [initial] value as zero.
+  /// When [duration] is [0], the animation is [loop] animation.
+  /// When [duration]is >[0]. the animation plays only for [duration] seconds.
+  final int duration;
+
   /// Creates Floating Bubbles in the Foreground to Any widgets.
   ///
   /// All Fields Are Required to make a new [Instance] of Bubbles.
@@ -29,6 +36,7 @@ class FloatingBubbles extends StatefulWidget {
     @required this.noOfBubbles,
     @required this.colorOfBubbles,
     @required this.sizeFactor,
+    this.duration = 0,
   })  : assert(
           noOfBubbles != null && noOfBubbles >= 10,
           'Number of Bubbles Cannot be null and not less than 10',
@@ -40,6 +48,9 @@ class FloatingBubbles extends StatefulWidget {
         assert(
           sizeFactor != null && sizeFactor > 0 && sizeFactor < 0.5,
           'Size factor cannot be null or greater than 0.5 or less than 0',
+        ),
+        assert(
+          duration >= 0 && duration != null,
         );
 
   @override
@@ -50,6 +61,10 @@ class _FloatingBubblesState extends State<FloatingBubbles> {
   /// Creating a Random object.
   final Random random = Random();
 
+  ///if [this] value is 0, animation is played, else animation is stopped.
+  ///Value of this is never changed when the duration is zero.
+  int checkToStopAnimation = 0;
+
   /// initialises a empty list of bubbles.
   final List<BubbleFloatingAnimation> bubbles = [];
 
@@ -58,25 +73,52 @@ class _FloatingBubblesState extends State<FloatingBubbles> {
     for (int i = 0; i < widget.noOfBubbles; i++) {
       bubbles.add(BubbleFloatingAnimation(random));
     }
+    if (widget.duration != 0)
+      Timer(Duration(seconds: widget.duration), () {
+        setState(() {
+          checkToStopAnimation = 1;
+        });
+      });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     /// Creates a Loop Animation of Bubbles that float around the screen from bottom to top.
-    return LoopAnimation(
-      tween: ConstantTween(1),
-      builder: (context, child, _) {
-        _simulateBubbles();
-        return CustomPaint(
-          painter: BubbleModel(
-            bubbles: bubbles,
-            color: widget.colorOfBubbles,
-            sizeFactor: widget.sizeFactor,
-          ),
-        );
-      },
-    );
+    /// If [duration] is 0, then the animation loops itself again and again.
+    /// IF[duration] is not 0, then the animation plays till the duration and stops.
+    return widget.duration == 0
+        ? LoopAnimation(
+            tween: ConstantTween(1),
+            builder: (context, child, value) {
+              _simulateBubbles();
+
+              return CustomPaint(
+                painter: BubbleModel(
+                  bubbles: bubbles,
+                  color: widget.colorOfBubbles,
+                  sizeFactor: widget.sizeFactor,
+                ),
+              );
+            },
+          )
+        : PlayAnimation(
+            duration: checkToStopAnimation == 0 ? Duration(seconds: widget.duration) : Duration.zero,
+            tween: ConstantTween(1),
+            builder: (context, child, value) {
+              _simulateBubbles();
+              if (checkToStopAnimation == 0)
+                return CustomPaint(
+                  painter: BubbleModel(
+                    bubbles: bubbles,
+                    color: widget.colorOfBubbles,
+                    sizeFactor: widget.sizeFactor,
+                  ),
+                );
+              else
+                return Container();
+            },
+          );
   }
 
   /// This Function checks whether the bubbles in the screen have to be restarted due to
