3863a28744d0823e6cca36e29e649662df2ec4cf
Poujhit <poujhit.dev@gmail.com>
2021-01-29 23:07:45 +0530

    feat: added named constructor alwaysRepeating()

--- lib/src/bubbles.dart
+++ lib/src/bubbles.dart
@@ -7,6 +7,7 @@ import 'package:simple_animations/simple_animations.dart';
 import 'bubble_floating_animation.dart';
 
 /// Creates Floating Bubbles in the Foreground of Any [widgets].
+// ignore: must_be_immutable
 class FloatingBubbles extends StatefulWidget {
   /// Number of Bubbles to be shown per second. Should be [> 10] and not [null].
   /// Whenever this value is changed, do a **Hot Restart** to see the Changes.
@@ -24,19 +25,20 @@ class FloatingBubbles extends StatefulWidget {
   final double sizeFactor;
 
   /// Number of [Seconds] the animation needs to draw on the screen.
-  /// [duration] is has [initial] value as zero.
-  /// When [duration] is [0], the animation is [loop] animation.
-  /// When [duration]is >[0]. the animation plays only for [duration] seconds.
-  final int duration;
+  /// If you want the bubbles to be floating always then use the constructor
+  /// `FloatingBubbles.alwaysRepeating()`.
+  int duration;
 
-  /// Creates Floating Bubbles in the Foreground to Any widgets.
+  /// Creates Floating Bubbles in the Foreground to Any widgets that plays for [duration] amount of time.
   ///
-  /// All Fields Are Required to make a new [Instance] of Bubbles.
+  /// All Fields Are Required to make a new [Instance] of FloatingBubbles.
+  /// If you want the bubbles to be floating always then use the constructor
+  /// `FloatingBubbles.alwaysRepeating()`.
   FloatingBubbles({
     @required this.noOfBubbles,
     @required this.colorOfBubbles,
     @required this.sizeFactor,
-    this.duration = 0,
+    @required this.duration,
   })  : assert(
           noOfBubbles != null && noOfBubbles >= 10,
           'Number of Bubbles Cannot be null and not less than 10',
@@ -52,6 +54,27 @@ class FloatingBubbles extends StatefulWidget {
         assert(
           duration >= 0 && duration != null,
         );
+
+  /// Creates Floating Bubbles that always floats and doesn't stop.
+  /// All Fields Are Required to make a new [Instance] of FloatingBubbles.
+  FloatingBubbles.alwaysRepeating({
+    @required this.noOfBubbles,
+    @required this.colorOfBubbles,
+    @required this.sizeFactor,
+  })  : assert(
+          noOfBubbles != null && noOfBubbles >= 10,
+          'Number of Bubbles Cannot be null and not less than 10',
+        ),
+        assert(
+          colorOfBubbles != null,
+          'Color of the bubble cannot be null',
+        ),
+        assert(
+          sizeFactor != null && sizeFactor > 0 && sizeFactor < 0.5,
+          'Size factor cannot be null or greater than 0.5 or less than 0',
+        ) {
+    duration = 0;
+  }
 
   @override
   _FloatingBubblesState createState() => _FloatingBubblesState();
@@ -73,7 +96,7 @@ class _FloatingBubblesState extends State<FloatingBubbles> {
     for (int i = 0; i < widget.noOfBubbles; i++) {
       bubbles.add(BubbleFloatingAnimation(random));
     }
-    if (widget.duration != 0)
+    if (widget.duration != 0 && widget.duration != null)
       Timer(Duration(seconds: widget.duration), () {
         setState(() {
           checkToStopAnimation = 1;
@@ -87,7 +110,7 @@ class _FloatingBubblesState extends State<FloatingBubbles> {
     /// Creates a Loop Animation of Bubbles that float around the screen from bottom to top.
     /// If [duration] is 0, then the animation loops itself again and again.
     /// If [duration] is not 0, then the animation plays till the duration and stops.
-    return widget.duration == 0
+    return widget.duration == 0 && widget.duration != null
         ? LoopAnimation(
             tween: ConstantTween(1),
             builder: (context, child, value) {
@@ -103,9 +126,7 @@ class _FloatingBubblesState extends State<FloatingBubbles> {
             },
           )
         : PlayAnimation(
-            duration: checkToStopAnimation == 0
-                ? Duration(seconds: widget.duration)
-                : Duration.zero,
+            duration: checkToStopAnimation == 0 ? Duration(seconds: widget.duration) : Duration.zero,
             tween: ConstantTween(1),
             builder: (context, child, value) {
               _simulateBubbles();
